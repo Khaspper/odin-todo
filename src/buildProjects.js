428dc71bf28bc409089bc8c328b1cddc597d550e
Mark <brandonnarciso24@gmail.com>
2025-05-15 18:28:44 -0700

    Finish Implementing a project object
    with functions to add tasks and format it following the S from SOLID

--- src/buildProjects.js
+++ src/buildProjects.js
@@ -10,30 +10,37 @@ function createDueDate() {
   return input;
 }
 
-function createNotes() {
+function createNotes(notes) {
   const input = document.createElement('textarea');
   input.rows = 20;
   input.cols = 60;
+  input.textContent = notes;
   return input;
 }
 
-function tasks() {
+function createTitle(title) {
+  const input = document.createElement('p');
+  input.textContent = title;
+  return input;
+}
+
+function buildTasks(title, notes) {
   return { 
     checkbox: createCheckbox(),
     dueDate: createDueDate(), 
-    notes: createNotes(),
-    title: '' 
+    notes: createNotes(notes),
+    title: createTitle(title)
   };
 }
 
 function formatTask(title, notes) {
-    const task = tasks();
+    const task = buildTasks(title, notes);
     const formatedTask = document.createElement('div');
     formatedTask.appendChild(task.checkbox);
     formatedTask.appendChild(task.dueDate);
     formatedTask.appendChild(task.notes);
     formatedTask.appendChild(task.title);
-    return formatTask;
+    return formatedTask;
 }
 
 export function project() {
@@ -42,5 +49,12 @@ export function project() {
     const formatedTask = formatTask(title, notes);
     todoList.push(formatedTask);
   };
-  return { addTask }
+  function printList() {
+    console.log('Start!')
+    for (const i of todoList) {
+      console.log(i);
+    }
+    console.log('Done!');
+  };
+  return { addTask, printList }
 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import './styles.css';
-import { project } from './task';
+import { project } from './buildProjects';
 
-console.log(taskItem.checkbox);
+const homeProject = project()
+//TODO: I want to display my todo's on the display card...
+//TODO: so starting off I think I should do event delegation first
+//TODO: So whenever they click on 'Home', 'Today', 'Week' or their other Projects
+//TODO: It's will get to be displayed on the display card
